826cd7fa0a2ee1135677f3837fced39a34582177
Vlad A. Ionescu <vladaionescu@users.noreply.github.com>
2021-03-30 18:07:39 -0700

    Increase retries.

--- util/resolver/retryhandler/retry.go
+++ util/resolver/retryhandler/retry.go
@@ -35,7 +35,7 @@ func New(f images.HandlerFunc, logger func([]byte)) images.HandlerFunc {
 				return descs, nil
 			}
 			// backoff logic
-			if backoff >= 8*time.Second {
+			if backoff >= 60*time.Second {
 				return nil, err
 			}
 			if logger != nil {
